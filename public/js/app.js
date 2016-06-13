3c3aa657367de875498e8a79f6350a61674adde2
Matt Stephens <matthewstephens@digidev.co.uk>
2016-06-13 14:38:44 +0100

    Ckeditor added

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,27 @@
 $(function() {
 
-	$('.ui.dropdown').dropdown({on: 'hover'})
+	// CKEDITOR
+	if ($('[name="body"]').length > 0) {
+		CKEDITOR.replace('body', {
+			skin: 'flat,/js/flat/',
+			customConfig: '/js/ckeditor.js'
+		})
+	}
+
+	// Semantic
+   $('.ui.calendar').calendar({
+		on: 'focus',
+		formatter: {
+			date: function (date, settings) {
+				if (!date) return '';
+				var day = ("0" + date.getDate()).slice(-2);
+				var month = ("0" + (date.getMonth() + 1)).slice(-2);
+				var year = date.getFullYear();
+				return  + year + '-' + month + '-' + day;
+			}
+		},
+		type: 'datetime'
+	});
+	$('.ui.dropdown').dropdown({on: 'focus'})
 
 });
---
+++ public/js/ckeditor.js
@@ -0,0 +1,19 @@
+CKEDITOR.editorConfig = function( config ) {
+
+    config.toolbar = [['Format'],['Bold','-','Italic'],['NumberedList','-','BulletedList','-','Blockquote'],['Link','-','Unlink','-','Anchor'],['Image','-','Table','-','HorizontalRule'],['Undo','-','Redo'],['Maximize'],['Source']];
+
+    // Remove some buttons provided by the standard plugins, which are
+    // not needed in the Standard(s) toolbar.
+    config.removeButtons = 'Underline,Subscript,Superscript';
+    config.contentsCss = '/css/app.css';
+    // Set the most common block elements.
+    config.format_tags = 'p;h2;h3;h4;div';
+    config.height = 300;
+    config.resize_maxWidth = 722;
+    config.removePlugins = 'elementspath';
+    config.allowedContent = true;
+
+    // Simplify the dialog windows.
+    config.removeDialogTabs = 'link:advanced';
+};
+CKEDITOR.dtd.$removeEmpty['i'] = false;
