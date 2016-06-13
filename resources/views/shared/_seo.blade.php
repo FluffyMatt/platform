762503a98547b53d2980eedeeeffc9598fdcc4f6
Matt Stephens <matthewstephens@digidev.co.uk>
2016-06-13 14:38:54 +0100

    SEO block added

---
+++ resources/views/shared/_seo.blade.php
@@ -0,0 +1,14 @@
+<div class="ui horizontal divider">
+<i class="sitemap icon"></i>
+SEO
+</div>
+
+<div class="field">
+	<label for="seo_title">Title</label>
+	<input type="text" name="seo_title" value="">
+</div>
+
+<div class="field">
+	<label for="seo_description">Description</label>
+	<textarea name="seo_description" value="" rows="2"></textarea>
+</div>
