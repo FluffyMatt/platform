1c511f1ea3e83cc85c3dbefa67436ef961b8cade
Matt Stephens <matthewstephens@digidev.co.uk>
2016-06-15 09:29:35 +0100

    Content Model added

---
+++ app/Content.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model
+{
+
+    protected $fillable = [
+        'title',
+        'seo_title',
+        'description',
+        'seo_description',
+        'body',
+        'status',
+        'published_at',
+        'slug'
+    ]
+
+    publinc function users() {
+        return $this->belongsToMany('App\User', 'content_users', 'user_id', 'content_id')
+    }
+
+}
