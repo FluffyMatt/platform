6ecf9be1dba2b3a28f0007eb91922fd610d40fbf
Ben Knowles <ben@digitalgarden.co>
2016-06-17 09:00:34 +0100

    Add active parent item helper and style

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -17,4 +17,14 @@ class AppHelper
 		return $active;
 	}
 
+	// Checks URL segments for a match to determine if given segment is an active parent
+	public static function is_active_parent($segment)
+	{
+		$active = '';
+		if (Request::is($segment) || Request::is($segment.'/*')) {
+			$active = 'active-parent';
+		}
+		return $active;
+	}
+
 }
