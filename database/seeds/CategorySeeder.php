48b82e58a2999c86268018f64ecda2a215ca81fd
Ben Knowles <ben@digitalgarden.co>
2016-06-22 09:52:58 +0100

    Category seeder

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+                'id' => 1,
+                'title' => 'Mobile Devices',
+                'slug' =>  'mobile-devices',
+                'parent_id' => null
+            ],
+            [
+                'id' => 2,
+                'title' => 'Smartphones',
+                'slug' =>  'smartphones',
+                'parent_id' => 1
+            ],
+            [
+                'id' => 3,
+                'title' => 'Apple',
+                'slug' =>  'apple',
+                'parent_id' => null
+            ],
+            [
+                'id' => 4,
+                'title' => 'iPhone 6s',
+                'slug' =>  'iphone-6s',
+                'parent_id' => 3
+            ],
+        ]);
+    }
+}
