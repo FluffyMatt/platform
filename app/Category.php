a74b5e3d3c8ce4fcbdb481117b92000afb6ddcc1
Ben Knowles <ben@digitalgarden.co>
2016-06-23 11:47:06 +0100

    Tree builder with recursive loop and method to retrieve top level or sub-level items.

--- app/Category.php
+++ app/Category.php
@@ -3,11 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Helpers\AppHelper;
 
 class Category extends Model
 {
 
 	public $timestamps = false;
+
+	public $tree = [];
+
+	public $level;
 
     protected $fillable = [
         'title',
@@ -26,5 +31,36 @@ class Category extends Model
     public function children() {
         return $this->hasMany('App\Category', 'parent_id')->orderBy('title');
     }
+
+	public static function items($parent_slug = null) {
+		if ($parent_slug) {
+			$parent = Category::where('slug', $parent_slug)->firstOrFail();
+			return $parent->children()->get();
+		} else {
+			return Category::whereNull('parent_id')->orderBy('title', 'asc')->get();
+		}
+	}
+
+	public static function tree($parent = null) {
+
+		$category = new Category();
+		$categories = Category::items($parent);
+
+		foreach ($categories as $c) {
+			$category->level = 0;
+			$category->treeLoop($c);
+		}
+
+		return $category->tree;
+	}
+
+	public function treeLoop($category) {
+
+		$this->tree[$category->id] = AppHelper::indent($this->level) . $category['title'];
+		$this->level++;
+		foreach ($category->children as $category) {
+			$this->treeLoop($category);
+		}
+	}
 
 }
