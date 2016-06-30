0eb1b445efa47b2dd5a5d8404e56d6595b0c62ef
Ben Knowles <ben@digitalgarden.co>
2016-06-30 16:43:59 +0100

    Add field IDs for targeting

--- resources/views/content/_form.blade.php
+++ resources/views/content/_form.blade.php
@@ -8,24 +8,24 @@
 
 			<div class="ui padded segment">
 
-				<div class="field">
+				<div id="content-title" class="field">
 					<label for="title">Title</label>
 					<input type="text" name="title" value="{{ old('title', @$content->title) }}">
 				</div>
 
-				<div class="field">
+				<div id="content-description" class="field">
 					<label for="description">Description</label>
 					<textarea name="description" rows="2">{{ old('description', @$content->description) }}</textarea>
 				</div>
 
-				<div class="field">
+				<div id="content-slug" class="field">
 					<label for="slug">URL</label>
 					<input type="text" name="slug" value="{{ old('slug', @$content->slug) }}">
 				</div>
 
-				<div class="field">
+				<div id="content-body" class="field">
 					<label for="body">Body</label>
-					<textarea type="textarea" name="body" >{{ old('body', @$content->body) }}</textarea>
+					<textarea name="body">{{ old('body', @$content->body) }}</textarea>
 				</div>
 
 			</div>
@@ -42,9 +42,9 @@
 
 			<div class="ui padded segment">
 
-			<div class="field hide">
+			<div id="content-type" class="field hide">
 				<label for="type">Type</label>
-				<select class="ui fluid dropdown" name="type" id="type">
+				<select class="ui fluid dropdown" name="type">
 					<option value=""></option>
 					@foreach ($options['types'] as $type)
 						<option value="{{ $type }}" @if(old('type', @$content->type) == $type) selected @endif>{{ ucfirst($type) }}</option>
@@ -52,22 +52,22 @@
 				</select>
 			</div>
 
-			<div class="field">
+			<div id="content-status" class="field">
 				<label for="status">Status</label>
-				<select class="ui fluid dropdown" name="status" id="status">
+				<select class="ui fluid dropdown" name="status">
 					<option value="draft" @if(old('status', @$content->status) == 'draft') selected @endif >Draft</option>
 					<option value="published" @if(old('status', @$content->status) == 'published') selected @endif >Published</option>
 				</select>
 			</div>
 
-			<div class="field">
+			<div id="content-published-at" class="field">
 				<label><i class="calendar icon"></i> Publish date</label>
 				<div class="ui calendar">
-					<input name="published_at" type="text" value="{{ old('published_at', @$content->published_at) }}" placeholder="">
+					<input accept=""name="published_at" type="text" value="{{ old('published_at', @$content->published_at) }}" placeholder="">
 				</div>
 			</div>
 
-			<div class="field">
+			<div id="content-users" class="field">
 				<label for="users">Authors</label>
 				<select name="users[]" class="ui fluid search dropdown authors" multiple>
 					@foreach ($options['users'] as $full_name => $id)
@@ -85,7 +85,7 @@
 				</select>
 			</div>
 
-			<div class="field">
+			<div id="content-categories" class="field">
 				<label for="categories">Categories</label>
 				<select name="categories[]" class="ui fluid search dropdown" multiple>
 					<option value=""></option>
