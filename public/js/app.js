8aa0e89bcff14f7683d7d76aae89dc95b19a7759
Matt Stephens <matthewstephens@digidev.co.uk>
2016-07-01 15:13:56 +0100

    Js diff functions

--- public/js/app.js
+++ public/js/app.js
@@ -43,4 +43,36 @@ $(function() {
 
 	$('.ui.accordion').accordion();
 
+	// Js Diff
+	$("#old-diff").each(function(){
+		var newVal = $('#new-diff').text();
+		$('#new-diff').text('');
+		var oldVal = $('#old-diff').text();
+		$('#old-diff').text('');
+		var diff = JsDiff.diffWords(oldVal, newVal);
+		diff.forEach(function(part){
+			var color = '';
+			var append;
+			if (part.added) {
+				writeDiff(part, 'new-diff', 'added');
+				//$('#new-diff').html($('#new-diff').html().replace(part.value, "<span class='added' style='color: #5cb85c;'>"+part.value+"</span>"));
+			}
+			else if (part.removed) {
+				writeDiff(part, 'old-diff', 'removed');
+				//$('#old-diff').html($('#old-diff').html().replace(part.value, "<span class='removed' style='color: #d9534f;'>"+part.value+"</span>"));
+			} else {
+				writeDiff(part, 'old-diff', '');
+				writeDiff(part, 'new-diff', '');
+			}
+		});
+	  });
+
+	  function writeDiff(part, element, status) {
+		  element = document.getElementById(element);
+		  var span = document.createElement('span');
+		  span.className = status;
+		  span.appendChild(document.createTextNode(part.value));
+		  element.appendChild(span);
+	  }
+
 });
