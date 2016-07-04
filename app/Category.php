85c9fcf18f68eac0f400149be49e5f502e811590
Ben Knowles <ben@digitalgarden.co>
2016-07-04 12:50:00 +0100

    Set route key for categories

--- app/Category.php
+++ app/Category.php
@@ -20,6 +20,11 @@ class Category extends Model
         'parent_id'
     ];
 
+	public function getRouteKeyName()
+	{
+	    return 'slug';
+	}
+
     public function content() {
         return $this->belongsToMany('App\Content');
     }
