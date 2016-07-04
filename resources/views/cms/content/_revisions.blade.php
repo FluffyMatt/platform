a5cffc82f42fe8308afe3fe2821abd5be9ae2ae9
Matt Stephens <matthewstephens@digidev.co.uk>
2016-07-04 09:32:47 +0100

    Fixed last broken link

--- resources/views/cms/content/_revisions.blade.php
+++ resources/views/cms/content/_revisions.blade.php
@@ -13,7 +13,7 @@
 					<div class="content">
 						@foreach($revisions as $revision)
 							<div class="ui vertical segment">
-								<b>{{ ucfirst($revision->fieldName()) }}</b> changed by <b>{{ $revision->userResponsible()->full_name }}</b> {{ $revision->created_at->diffForHumans() }} (<a href="/content/revision/{{ $revision->id }}">{{ $revision->created_at->toDayDateTimeString() }}</a>)
+								<b>{{ ucfirst($revision->fieldName()) }}</b> changed by <b>{{ $revision->userResponsible()->full_name }}</b> {{ $revision->created_at->diffForHumans() }} (<a href="/cms/content/revision/{{ $revision->id }}">{{ $revision->created_at->toDayDateTimeString() }}</a>)
 							</div>
 						@endforeach
 					</div>
