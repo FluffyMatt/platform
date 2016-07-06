11d1d7615f960be6645d569b269143b56a24daed
Ben Knowles <ben@digitalgarden.co>
2016-07-06 09:40:45 +0100

    Comment request - lock down status changing to admin/editor only

---
+++ app/Http/Requests/CommentRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Request;
+
+class CommentRequest extends Request
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+		$current_slug = \Route::input('content');
+
+        return [
+            'message' => "required"
+        ];
+    }
+
+	public function all()
+	{
+		$data = parent::all();
+
+		if (!Auth::user()->admin && !Auth::user()->editor) {
+			unset($data['status']);
+		}
+
+		return $data;
+	}
+}
