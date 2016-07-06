823c0eb49498e1b6cfd5896456f95debb719fef1
Ben Knowles <ben@digitalgarden.co>
2016-07-06 09:27:59 +0100

    Comment model

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+	const status = ['pending', 'approved', 'rejected', 'spam', 'private'];
+
+    protected $fillable = [
+		'content_id',
+        'message',
+        'status'
+    ];
+
+    public function content()
+    {
+      return $this->belongsTo('App\Content');
+    }
+
+    public function user()
+    {
+      return $this->belongsTo('App\User');
+    }
 }
