d54abbd8e8d2e91793e0771ca1e515029ac6926d
Ben Knowles <ben@digitalgarden.co>
2016-07-06 14:18:20 +0100

    Remove comment count column, it clashes with relation, we'll just real-time this calculation for now

--- database/migrations/2016_06_03_142952_create_content_table.php
+++ database/migrations/2016_06_03_142952_create_content_table.php
@@ -24,7 +24,6 @@ class CreateContentTable extends Migration
             $table->string('description');
             $table->string('seo_description');
             $table->text('body');
-            $table->integer('comments');
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
             $table->index(['title', 'parent_id']);
