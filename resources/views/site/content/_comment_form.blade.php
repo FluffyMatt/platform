3e701f5a2830567fb2ac67eaf50b6489e46f9300
Ben Knowles <ben@digitalgarden.co>
2016-07-07 13:21:08 +0100

    Front-end comment form

---
+++ resources/views/site/content/_comment_form.blade.php
@@ -0,0 +1,11 @@
+<form id="comment-form" class="ui reply form" action="/comments" method="post">
+
+	<div id="comment-message" class="field">
+		<textarea name="message" class="unfocused"></textarea>
+	</div>
+
+	<button id="save" class="ui blue labeled submit icon button">
+		<i class="icon edit"></i> Post comment
+	</button>
+
+</form>
