f1fe97ec836f1ce6120eba33768b26f51b6f1b01
Ben Knowles <ben@digitalgarden.co>
2016-07-07 14:21:58 +0100

    Improve comment form active state

--- resources/views/site/content/_comment_form.blade.php
+++ resources/views/site/content/_comment_form.blade.php
@@ -1,7 +1,7 @@
 <form id="comment-form" class="ui reply form" action="/comments" method="post">
 
 	<div id="comment-message" class="field">
-		<textarea name="message" class="unfocused"></textarea>
+		<textarea name="message" class="inactive"></textarea>
 	</div>
 
 	<button id="save" class="ui blue labeled submit icon button">
