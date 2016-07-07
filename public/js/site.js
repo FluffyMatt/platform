e995bb5c2b0ae2a5f03f0598d389fb42b8eac35b
Ben Knowles <ben@digitalgarden.co>
2016-07-07 13:20:15 +0100

    Contextual classes on comment textarea

--- public/js/site.js
+++ public/js/site.js
@@ -1,5 +1,12 @@
 $(function() {
 
-
+	$('#comment-message textarea').focusin(function() {
+		$(this).addClass('focused');
+		$(this).removeClass('unfocused');
+	});
+	$('#comment-message textarea').focusout(function() {
+		$(this).removeClass('focused');
+		$(this).addClass('unfocused');
+	});
 
 });
