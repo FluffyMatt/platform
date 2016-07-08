5585c845274a3f402ee037e5b3513ba96038aaa5
Ben Knowles <ben@digitalgarden.co>
2016-07-08 16:47:38 +0100

    Add user id field and prefix note fields with comment

--- resources/views/cms/comments/_form_notes.blade.php
+++ resources/views/cms/comments/_form_notes.blade.php
@@ -1,6 +1,8 @@
-<input type="hidden" name="status" value="private">
+<input type="hidden" name="comment[status]" value="private">
+
+<input type="hidden" name="comment[user_id]" value="{{ Auth::user()->id }}">
 
 <div id="content-note" class="field">
-	<label for="message">Leave a new note</label>
-	<textarea name="message"></textarea>
+	<label for="comment[message]">Leave a new note</label>
+	<textarea name="comment[message]"></textarea>
 </div>
