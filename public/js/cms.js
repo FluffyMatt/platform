5eb7c137c7d276ec2b1eec9520a0572a7d5c3c83
Matt Stephens <matthewstephens@digidev.co.uk>
2016-07-11 16:41:15 +0100

    JS for file uploads

--- public/js/cms.js
+++ public/js/cms.js
@@ -81,14 +81,125 @@ $(function() {
 				writeDiff(part, 'new-diff', '');
 			}
 		});
-	  });
+	});
 
-	  function writeDiff(part, element, status) {
-		  element = document.getElementById(element);
-		  var span = document.createElement('span');
-		  span.className = status;
-		  span.appendChild(document.createTextNode(part.value));
-		  element.appendChild(span);
-	  }
+	function writeDiff(part, element, status) {
+	  element = document.getElementById(element);
+	  var span = document.createElement('span');
+	  span.className = status;
+	  span.appendChild(document.createTextNode(part.value));
+	  element.appendChild(span);
+	}
+
+	// File uploads
+	$('.button-upload').click(function() {
+	  $('#file-upload').click();
+	})
+
+	$('#file').change(function() {
+		$('.extra.content').children().hide();
+		$(this).parent().parent().prepend('<i class="green upload icon"></i> New file attached');
+	});
+
+	var rowId = 0;
+	var images =  [
+		'image/png',
+		'image/gif',
+		'image/jpg',
+		'image/jpeg'
+	];
+
+	$('#file-upload').change(function() {
+		$(this).parent().addClass('loading');
+		$(this).css('height', '80px');
+		$(this).parent().css('height', '80px');
+
+		for (var i = 0; i < this.files.length; i++) {
+			var formData = new FormData();
+			var file = this.files[i];
+			var reader = new FileReader();
+
+			reader.onload = (function(file, i) {
+				rowId++;
+				formData.append('file', file);
+				upload(formData, rowId);
+				var row = document.createElement('tr');
+				row.id = rowId;
+				return function(e) {
+					if (images.indexOf(file.type) > -1) {
+						var thumb = '<img src="'+this.result+'"/>';
+					} else {
+						var fileType = file.name.substr(file.name.indexOf('.')+1);
+						var thumb = '<i class="file '+fileType+' icon outline huge"></i>';
+					}
+					row.innerHTML = '<td><div class="ui card"><div class="ui tiny centered image">'+thumb+'</div><div data-percent="" class="ui bottom attached tiny indicating progress"><div class="bar"></div></div></td><td>'+file.name+'</td><td>'+file.type+'</td><td>'+humanFileSize(file.size)+'</td><td></td>';
+					document.getElementById('uploads').insertBefore(row, null);
+				}
+			})(file, i)
+
+			reader.readAsDataURL(file);
+
+			reader.onloadend = function() {
+				$('#uploads').removeClass('hide');
+				$('.loading').removeClass('loading');
+			}
+
+		}
+
+		if (reader.readyState == '2') {
+			$(this).parent().removeClass('loading');
+		}
+
+	})
+
+	function humanFileSize(size) {
+	    var i = Math.floor( Math.log(size) / Math.log(1024) );
+	    return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
+	};
+
+	function upload(file, id) {
+		$.ajax({
+			headers: { 'X-CSRF-Token' : $('input[name="_token"]').val() },
+			type:'POST',
+			url: '/cms/files/upload',
+			data:file,
+			cache:false,
+			contentType: false,
+			processData: false,
+
+			xhr: function() {
+				var myXhr = $.ajaxSettings.xhr();
+				if(myXhr.upload){
+				    myXhr.upload.addEventListener('progress',function progress(e){
+					    if(e.lengthComputable){
+					        var max = e.total;
+					        var current = e.loaded;
+
+					        var percentage = (current * 100)/max;
+							$('#'+id+' .ui.progress').progress({'percent': percentage});
+					    }
+					 }, false);
+				}
+				return myXhr;
+	        },
+
+			success:function(data){
+				data = $.parseJSON(data);
+				if (data.error) {
+					$('#'+id).addClass('negative');
+					$('#'+id).children('td').eq(4).html('Error - '+data.error.message);
+					$('#'+id+' .ui.progress').removeClass('success').addClass('error');
+				} else {
+					$('#'+id).addClass('positive');
+					$('#'+id).children('td').eq(4).html('<a class="ui button" target="_blank" href="/cms/files/'+data+'/edit">Edit</a>');
+				}
+			},
+
+			error: function(data){
+			  $('#'+id).addClass('negative');
+			}
+		});
+  	};
+
 
 });
