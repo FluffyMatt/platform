49e0a6a54077799ad49a5fee91ccffa5cfc362ce
Ben Knowles <ben@digitalgarden.co>
2016-07-11 15:47:23 +0100

    Add comments enabled/disabled to seed content

--- database/seeds/ContentSeeder.php
+++ database/seeds/ContentSeeder.php
@@ -86,6 +86,7 @@ class ContentSeeder extends Seeder
 
 <p>Ne sea dolor prodesset. Ei quo pertinax necessitatibus. Debitis democritum reformidans eu qui. In perpetua dignissim adversarium has, est et amet aliquam, ex affert pertinax dissentiet eum.</p>
 ',
+				'commentable' => true,
 				'published_at' => '2016-07-04 11:41',
 				'created_at' => '2016-07-04 11:41:23'
             ],
@@ -163,6 +164,7 @@ class ContentSeeder extends Seeder
 
 <p>Ne sea dolor prodesset. Ei quo pertinax necessitatibus. Debitis democritum reformidans eu qui. In perpetua dignissim adversarium has, est et amet aliquam, ex affert pertinax dissentiet eum.</p>
 ',
+				'commentable' => true,
 				'published_at' => '2016-07-04 11:41',
 				'created_at' => '2016-07-04 11:41:23'
             ],
