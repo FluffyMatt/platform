3ad1a4ab12e41a18d52d07e17517ea9b1c9d569b
Ben Knowles <ben@digitalgarden.co>
2016-07-12 11:50:31 +0100

    Update front-end Semantic UI

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -5,7 +5,7 @@
         @if (App::environment('local'))
             <link rel="stylesheet" href="/css/semantic.min.css">
         @else
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.1/semantic.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css">
         @endif
         <link rel="stylesheet" href="/css/site.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -45,7 +45,7 @@
             <script src="/js/semantic.min.js"></script>
         @else
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.1/semantic.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.js"></script>
         @endif
         <script src="/js/site.js"></script>
 
